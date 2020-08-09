2d155af340b3fcd762f0eed2a7d5763017906e6d
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-08 20:04:22 -0700

    update

--- client/src/components/PortfolioCard/index.js
+++ client/src/components/PortfolioCard/index.js
@@ -5,46 +5,57 @@ import "./style.css";
 
 function PortfolioCard(props) {
   return (
-    <div>
-      <div className="uk-card uk-card-default portfolio-card">
-        <div className="uk-card-body portfolio-card-body">
-          <div
-            className="uk-position-relative uk-visible-toggle uk-light"
-            tabIndex="-1"
-            uk-slideshow="true"
-          >
-            <ul className="uk-slideshow-items">
-              <li>
-                <h3 className="uk-card-title project-title">{props.title}</h3>
-                <p className="project-description">{props.about}</p>
-              </li>
-
-              <img src={props.imgURL} alt="" uk-cover="true" />
-              {props.lEx.map((item) => {
-                return <li title={item.learningExp}>{item.learningExp}</li>;
-              })}
-            </ul>
-            <a
-              className="uk-position-top-right uk-position-small uk-hidden-hover"
-              href="#"
-              uk-slidenav-next="true"
-              uk-slideshow-item="next"
-            ></a>
-          </div>
+    <div className="uk-card uk-card-default portfolio-card">
+      <div id={props.repoURL} uk-modal>
+        <div id={props.repoURL} class="uk-modal-dialog uk-modal-body">
+          <img src={props.gif} width="" height="" alt="" uk-img />
+          <button class="uk-modal-close" type="button"></button>
         </div>
-        <div className="uk-card-footer portfolio-card-footer">
-          <div className="uk-text-center">
-            <a href={props.siteURL} target="_blank" className="btn-link">
-              <button className="uk-button uk-button-default portfolio-btn">
-                View Website
-              </button>
-            </a>
-            <a href={props.repoURL} target="_blank" className="btn-link">
-              <button className="uk-button uk-button-default portfolio-btn">
-                GitHub Repo
-              </button>
-            </a>
-          </div>
+      </div>
+      <div className="uk-card-body portfolio-card-body">
+        <div
+          className="uk-position-relative uk-visible-toggle uk-light"
+          tabIndex="-1"
+          uk-slideshow="true"
+        >
+          <ul className="uk-slideshow-items">
+            <li>
+              <h3 className="uk-card-title project-title">
+                {props.title}
+                <button
+                  href={"#" + props.repoURL}
+                  type="button"
+                  class="color"
+                  uk-icon="tv"
+                  uk-toggle
+                />
+              </h3>
+              <p className="project-description">{props.about}</p>
+            </li>
+            {props.lEx.map((item) => {
+              return <li title={item.learningExp}>{item.learningExp}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="uk-card-footer portfolio-card-footer">
+        <div className="uk-text-center">
+          <a href={props.siteURL} target="_blank" className="btn-link">
+            <button
+              className="uk-button uk-button-default portfolio-btn "
+              uk-icon="world"
+            >
+              View Website{"    "}
+            </button>
+          </a>
+          <a href={props.repoURL} target="_blank" className="btn-link">
+            <button
+              className="uk-button uk-button-default portfolio-btn "
+              uk-icon="github"
+            >
+              GitHub Repo{"    "}
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- client/src/routes/Portfolio.js
+++ client/src/routes/Portfolio.js
@@ -45,7 +45,7 @@ function Portfolio() {
       <CardContainer>
         <h1>Front End Applications</h1>
         <div
-          className="uk-child-width-1-3@m uk-grid-match"
+          className="uk-child-width-1-2@m uk-grid-match"
           uk-grid="true"
           id="portfolio-section"
           uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 100; repeat: false"
@@ -66,49 +66,65 @@ function Portfolio() {
         </div>
       </CardContainer>
       <CardContainer>
-        <h1>Back End Applications</h1>
-        <div
-          className="uk-child-width-1-3@m uk-grid-match"
-          uk-grid="true"
-          id="portfolio-section"
-          uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 100; repeat: false"
-        >
-          {back.map((item) => {
-            return (
-              <PortfolioCard
-                title={item.name}
-                about={item.description}
-                repoURL={item.githubCode}
-                siteURL={item.githubRepo}
-                lEx={item.learningExp}
-                imgURL={item.gif}
-                key={item._id}
-              />
-            );
-          })}
+        <div class="uk-height-medium">
+          <div class="js-wrapper uk-background-muted uk-margin-auto">
+            <div uk-overflow-auto="selContainer: .uk-height-medium; selContent: .js-wrapper">
+              <div class="uk-grid-large" uk-grid>
+                <h1>Back End Applications</h1>
+                <div
+                  className="uk-child-width-1-2@m uk-grid-match "
+                  uk-grid="true"
+                  id="portfolio-section"
+                  uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 100; repeat: false"
+                >
+                  {back.map((item) => {
+                    return (
+                      <PortfolioCard
+                        title={item.name}
+                        about={item.description}
+                        repoURL={item.githubCode}
+                        siteURL={item.githubRepo}
+                        lEx={item.learningExp}
+                        imgURL={item.gif}
+                        key={item._id}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </CardContainer>
       <CardContainer>
-        <h1>Full Stack Applications</h1>
-        <div
-          className="uk-child-width-1-3@m uk-grid-match"
-          uk-grid="true"
-          id="portfolio-section"
-          uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 100; repeat: false"
-        >
-          {full.map((item) => {
-            return (
-              <PortfolioCard
-                title={item.name}
-                about={item.description}
-                repoURL={item.githubCode}
-                siteURL={item.githubRepo}
-                lEx={item.learningExp}
-                imgURL={item.gif}
-                key={item._id}
-              />
-            );
-          })}
+        <div class="uk-height-medium">
+          <div class="js-wrapper uk-background-muted">
+            <div uk-overflow-auto="selContainer: .uk-height-medium; selContent: .js-wrapper">
+              <div class="uk-grid-medium" uk-grid>
+                <h1>Full Stack Applications</h1>
+                <div
+                  className="uk-child-width-1-2@m uk-grid-match uk-margin-auto"
+                  uk-grid="true"
+                  id="portfolio-section"
+                  uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 100; repeat: false"
+                >
+                  {full.map((item) => {
+                    return (
+                      <PortfolioCard
+                        title={item.name}
+                        about={item.description}
+                        repoURL={item.githubCode}
+                        siteURL={item.githubRepo}
+                        lEx={item.learningExp}
+                        imgURL={item.gif}
+                        key={item._id}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </CardContainer>
     </>
