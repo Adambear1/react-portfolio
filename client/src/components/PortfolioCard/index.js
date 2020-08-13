df8caca956d8859165a58322bba23e501932f348
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-13 12:42:51 -0700

    update

--- client/src/components/PortfolioCard/index.js
+++ client/src/components/PortfolioCard/index.js
@@ -5,61 +5,95 @@ import "./style.css";
 
 function PortfolioCard(props) {
   return (
-    <div className="uk-card uk-card-default portfolio-card">
-      <div id={props.repoURL} uk-modal>
+    <>
+      <div className="uk-card uk-card-default portfolio-card">
+        {/* <div id={props.repoURL} uk-modal>
         <div class="uk-modal-dialog uk-modal-body">
           <img src={props.gif} width="" height="" alt="" uk-img />
           <button class="uk-modal-close" type="button"></button>
         </div>
-      </div>
-      <div className="uk-card-body portfolio-card-body">
-        <div
-          className="uk-position-relative uk-visible-toggle uk-light"
-          tabIndex="-1"
-          uk-slideshow="true"
-        >
-          <ul className="uk-slideshow-items">
-            <li>
-              <h3 className="uk-card-title project-title">
-                {props.title}
+      </div> */}
+        <div className="uk-card-body portfolio-card-body">
+          <div
+            className="uk-position-relative uk-visible-toggle uk-light"
+            tabIndex="-1"
+            uk-slideshow="true"
+          >
+            <ul className="uk-slideshow-items">
+              <li>
+                <h3 className="uk-card-title project-title">
+                  {props.title}
 
-                <button
+                  {/* <button
                   href={"#" + props.repoURL}
                   type="button"
                   class="color"
                   uk-icon="tv"
                   uk-toggle
-                />
-              </h3>
-              <p className="project-description">{props.about}</p>
-            </li>
-            {props.lEx.map((item) => {
-              return <li title={item.learningExp}>{item.learningExp}</li>;
-            })}
-          </ul>
+                /> */}
+                  <button
+                    class="uk-button uk-button-default uk-margin-small-right"
+                    type="button"
+                    uk-toggle="target: #modal-example"
+                  >
+                    Open
+                  </button>
+                </h3>
+                <p className="project-description">{props.about}</p>
+              </li>
+              {props.lEx.map((item) => {
+                return <li title={item.learningExp}>{item.learningExp}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="uk-card-footer portfolio-card-footer">
+          <div className="uk-text-center">
+            <a href={props.siteURL} target="_blank" className="btn-link">
+              <button
+                className="uk-button uk-button-default portfolio-btn "
+                uk-icon="world"
+              >
+                View Website{"    "}
+              </button>
+            </a>
+            <a href={props.repoURL} target="_blank" className="btn-link">
+              <button
+                className="uk-button uk-button-default portfolio-btn "
+                uk-icon="github"
+              >
+                GitHub Repo{"    "}
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="uk-card-footer portfolio-card-footer">
-        <div className="uk-text-center">
-          <a href={props.siteURL} target="_blank" className="btn-link">
+      <div id="modal-example" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+          <h2 class="uk-modal-title">Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="uk-text-right">
             <button
-              className="uk-button uk-button-default portfolio-btn "
-              uk-icon="world"
+              class="uk-button uk-button-default uk-modal-close"
+              type="button"
             >
-              View Website{"    "}
+              Cancel
             </button>
-          </a>
-          <a href={props.repoURL} target="_blank" className="btn-link">
-            <button
-              className="uk-button uk-button-default portfolio-btn "
-              uk-icon="github"
-            >
-              GitHub Repo{"    "}
+            <button class="uk-button uk-button-primary" type="button">
+              Save
             </button>
-          </a>
+          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
